aebd751fbe0512f81092657b3880478bf9f0fa5f
Sumit Modak <152054612+notSumitGG@users.noreply.github.com>
2024-03-10 18:38:01 +0530

    added 02-Sorting_and_Searching/027-maximum_subarray_sum.rs

---
+++ 02-Sorting_and_Searching/027-maximum_subarray_sum.rs
@@ -0,0 +1,64 @@
+fn main() {
+    let mut token = Tokenizer::new();
+    let n = token.next::<usize>();
+    let mut max = i64::MIN;
+    let mut maxsum = i64::MIN;
+    let mut cursum = 0i64;
+    for _ in 0..n {
+        let value = token.next::<i64>();
+        cursum += value;
+        if value > max {
+            max = value;
+        }
+        if cursum < 0 {
+            cursum = 0;
+        } else if cursum > maxsum {
+            maxsum = cursum;
+        }
+    }
+    if maxsum > 0 {
+        println!("{maxsum}");
+    } else {
+        println!("{max}");
+    }
+}
+
+struct Tokenizer {
+    buf: Vec<String>,
+    i: usize,
+}
+
+impl Tokenizer {
+    pub fn new() -> Self {
+        return Tokenizer {
+            buf: Vec::<String>::new(),
+            i: 0,
+        };
+    }
+
+    fn read_line(&mut self) {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        self.buf = s.split_whitespace().map(str::to_string).collect();
+        self.i = 0;
+    }
+
+    pub fn next<T: std::str::FromStr>(&mut self) -> T
+    where
+        T::Err: std::fmt::Debug,
+    {
+        while self.i == self.buf.len() {
+            self.read_line();
+        }
+        let t = self.buf[self.i].parse().unwrap();
+        self.i += 1;
+        return t;
+    }
+
+    #[allow(dead_code)]
+    pub fn next_line(&self) -> String {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        return s;
+    }
+}

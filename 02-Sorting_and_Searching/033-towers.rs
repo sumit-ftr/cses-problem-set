7fcf7ee07391f01520d1a8a218e6981e1215794f
Sumit Modak <152054612+sumit-ftr@users.noreply.github.com>
2024-03-24 12:09:54 +0530

    optimized 02-Sorting_and_Searching/033-towers.rs

--- 02-Sorting_and_Searching/033-towers.rs
+++ 02-Sorting_and_Searching/033-towers.rs
@@ -1,27 +1,62 @@
-use std::collections::BTreeMap;
 use std::io::Write;
+
+trait UpperBound {
+    type Item;
+    fn upper_bound(&mut self, value: Self::Item) -> Option<&mut Self::Item>;
+}
+
+impl<T: std::cmp::PartialOrd> UpperBound for Vec<T> {
+    type Item = T;
+    fn upper_bound(&mut self, value: T) -> Option<&mut Self::Item> {
+        let (mut low, mut high) = (0isize, self.len() as isize - 1);
+        let mut res = self.len();
+        if res == 0 || self[high as usize] <= value {
+            return None;
+        }
+        while low <= high {
+            let mid = (low + high) / 2;
+            if self[mid as usize] > value {
+                res = mid as usize;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        Some(&mut self[res])
+    }
+}
 
 fn main() {
     let mut token = Scanner::new(std::io::stdin().lock());
     let mut out = std::io::BufWriter::new(std::io::stdout().lock());
     let n = token.next::<usize>();
-    let mut set = BTreeMap::<u32, u32>::new();
-    let mut sum = 0usize;
+    let mut cubes = Vec::<u32>::with_capacity(n);
     for _ in 0..n {
         let x = token.next::<u32>();
-        if let Some((&value, count)) = set.range_mut(x + 1..).next() {
-            if *count == 1 {
-                set.remove(&value);
-            } else {
-                *count -= 1;
-            }
+        if let Some(value) = cubes.upper_bound(x) {
+            *value = x;
         } else {
-            sum += 1;
+            cubes.push(x);
         }
-        set.entry(x).and_modify(|v| *v += 1).or_insert(1);
     }
-    writeln!(out, "{sum}").unwrap();
+    writeln!(out, "{}", cubes.len()).unwrap();
 }
+
+// let mut set = BTreeMap::<u32, u32>::new();
+// for _ in 0..n {
+//     let x = token.next::<u32>();
+//     if let Some((&value, count)) = set.range_mut(x + 1..).next() {
+//         if *count == 1 {
+//             set.remove(&value);
+//         } else {
+//             *count -= 1;
+//         }
+//     } else {
+//         sum += 1;
+//     }
+//     set.entry(x).and_modify(|v| *v += 1).or_insert(1);
+// }
+// writeln!(out, "{sum}").unwrap();
 
 pub struct Scanner<R> {
     reader: R,

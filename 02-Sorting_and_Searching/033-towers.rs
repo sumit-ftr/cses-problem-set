0fc04baa91d4737a3236c989242d1e493de4c8f0
Sumit Modak <152054612+sumit-ftr@users.noreply.github.com>
2024-03-24 09:45:55 +0530

    added 02-Sorting_and_Searching/033-towers.rs

---
+++ 02-Sorting_and_Searching/033-towers.rs
@@ -0,0 +1,56 @@
+use std::collections::BTreeMap;
+use std::io::Write;
+
+fn main() {
+    let mut token = Scanner::new(std::io::stdin().lock());
+    let mut out = std::io::BufWriter::new(std::io::stdout().lock());
+    let n = token.next::<usize>();
+    let mut set = BTreeMap::<u32, u32>::new();
+    let mut sum = 0usize;
+    for _ in 0..n {
+        let x = token.next::<u32>();
+        if let Some((&value, count)) = set.range_mut(x + 1..).next() {
+            if *count == 1 {
+                set.remove(&value);
+            } else {
+                *count -= 1;
+            }
+        } else {
+            sum += 1;
+        }
+        set.entry(x).and_modify(|v| *v += 1).or_insert(1);
+    }
+    writeln!(out, "{sum}").unwrap();
+}
+
+pub struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: std::io::BufRead> Scanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = std::str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}

276b4ba8679c3577324f10a2eaacdf74aae93c1d
Sumit Modak <152054612+sumit-ftr@users.noreply.github.com>
2024-03-31 23:01:10 +0530

    now scrape fetches all hacking pages & fixed serializing error

--- scrape/src/lib.rs
+++ scrape/src/lib.rs
@@ -1,5 +1,6 @@
 use reqwest::{self, cookie::Jar, Client, Url};
 use scraper::{Html, Selector};
+use serde;
 use serde_json;
 use std::collections::BTreeMap;
 use std::error::Error;
@@ -17,6 +18,7 @@ pub struct Config {
 impl Config {
     pub async fn new(mut a: std::env::Args) -> Result<Self, Box<dyn Error>> {
         a.next();
+        // matching first argument (problem number)
         let (pno, mut url) = match a.next() {
             Some(num) => {
                 let num = num.parse::<usize>()?;
@@ -25,6 +27,7 @@ impl Config {
             None => return Err(Box::<dyn Error>::from("Error: Problem Number Not Found")),
         };
 
+        // setting up client with the cookie
         let cookie = tokio::fs::read_to_string("userdata/cookie").await?;
         let jar = Jar::default();
         jar.add_cookie_str(&cookie[..cookie.len() - 1], &url.parse::<Url>().unwrap());
@@ -32,8 +35,9 @@ impl Config {
             .cookie_store(true)
             .cookie_provider(std::sync::Arc::new(jar))
             .build()?;
-
         Self::authenticate(&client).await?;
+
+        // parsing solutions of the first hacking page
         let mut top_rust = BTreeMap::<(u8, u16), String>::new();
         let mut top_time = BTreeMap::<(u8, u16), String>::new();
         let rng = Self::get_range(&client, &url, &mut top_rust, &mut top_time).await?;
@@ -86,17 +90,41 @@ impl Config {
     ) -> Result<usize, Box<dyn Error>> {
         let res_body = client.get(url).send().await?.text().await?;
         let fragment = Html::parse_fragment(&res_body);
+        // takes the response body and gets the fastest solutions
+        Self::get_fastest_by_page(&res_body, top_rust, top_time).await?;
+        println!("parsed 1st page successfully");
+
+        let rng = fragment
+            .select(&Selector::parse("a")?)
+            .nth(17)
+            .unwrap()
+            .inner_html()
+            .parse::<usize>()
+            .unwrap();
+
+        // Ok(rng)
+        Ok(5)
+    }
+
+    pub async fn get_fastest_by_page(
+        res_body: &String,
+        top_rust: &mut BTreeMap<(u8, u16), String>,
+        top_time: &mut BTreeMap<(u8, u16), String>,
+    ) -> Result<(), Box<dyn Error>> {
+        let fragment = Html::parse_fragment(res_body);
 
         // updating the BTreeMaps
         let selector = Selector::parse("td")?;
         let mut submissions = fragment.select(&selector);
         while let Some(_) = submissions.next() {
             submissions.next();
+            // finding values from the html
             let lang = submissions.next().unwrap().inner_html();
             let time = submissions.next().unwrap().inner_html();
             let chr_cnt = submissions.next().unwrap().inner_html();
             let endpoint = Html::parse_fragment(&submissions.next().unwrap().inner_html());
 
+            // computing the key & values of the BTreeMaps
             let time = time[..time.len() - 2].parse::<f64>().unwrap() as u8;
             let len = chr_cnt[..chr_cnt.len() - 4].parse::<usize>().unwrap() as u16;
             let value = format!(
@@ -109,32 +137,32 @@ impl Config {
                     .unwrap()
             );
 
+            // inserting values into BTreeMaps
             if lang == "Rust" {
                 top_rust.insert((time, len), value);
             } else {
+                if top_time.len() > 63 {
+                    top_time.pop_last();
+                }
                 top_time.insert((time, len), value);
             }
         }
-
-        let rng = fragment
-            .select(&Selector::parse("a")?)
-            .nth(17)
-            .unwrap()
-            .inner_html()
-            .parse::<usize>()
-            .unwrap();
-
-        Ok(rng)
-    }
-
-    pub async fn get_fastest(&mut self) -> Result<(), Box<dyn Error>> {
         Ok(())
     }
 
-    pub async fn get_fastest_by_page(&mut self) -> Result<(), Box<dyn Error>> {
-        // while let Some(val) = it.next() {
-        //     println!("{:#?}", val.html());
-        // }
+    pub async fn get_fastest(&mut self) -> Result<(), Box<dyn Error>> {
+        for i in 2..=self.rng {
+            let res_body = self
+                .client
+                .get(format!("{}{i}", self.url))
+                .send()
+                .await?
+                .text()
+                .await?;
+            // takes the response body and gets the fastest solutions
+            Self::get_fastest_by_page(&res_body, &mut self.top_rust, &mut self.top_time).await?;
+            println!("parsed {i}th page successfully");
+        }
         Ok(())
     }
 
@@ -143,18 +171,50 @@ impl Config {
         // and the user couldn't able to get the solutions, in that case the user
         // can get all the solutions without scraping all the solution webpages
         tokio::fs::create_dir_all(format!("solutions/{}/", self.pno)).await?;
-        let mut ftop_rust = File::create(&format!("solutions/{}/top_rust.json", self.pno)).await?;
-        let mut ftop_time = File::create(&format!("solutions/{}/top_time.json", self.pno)).await?;
-        ftop_rust
-            .write_all(serde_json::to_string(&self.top_rust).unwrap().as_bytes())
-            .await?;
-        ftop_time
-            .write_all(serde_json::to_string(&self.top_time).unwrap().as_bytes())
-            .await?;
+        let mut ftrs = File::create(&format!("solutions/{}/top_rust.json", self.pno)).await?;
+        let mut fttm = File::create(&format!("solutions/{}/top_time.json", self.pno)).await?;
+
+        // creating new vectors of SolnEntry
+        #[derive(serde::Serialize)]
+        struct SolnEntry {
+            time: u8,
+            chars: u16,
+            url: String,
+        }
+        let top_rust_vec = self
+            .top_rust
+            .clone()
+            .into_iter()
+            .map(|((time, chars), url)| SolnEntry { time, chars, url })
+            .collect::<Vec<SolnEntry>>();
+        let top_time_vec = self
+            .top_time
+            .clone()
+            .into_iter()
+            .map(|((time, chars), url)| SolnEntry { time, chars, url })
+            .collect::<Vec<SolnEntry>>();
+
+        // writing to json files
+        ftrs.write_all(
+            serde_json::to_string_pretty(&top_rust_vec)
+                .unwrap()
+                .as_bytes(),
+        )
+        .await?;
+        fttm.write_all(
+            serde_json::to_string_pretty(&top_time_vec)
+                .unwrap()
+                .as_bytes(),
+        )
+        .await?;
         Ok(())
     }
 
     pub async fn write_all_files(&self) -> Result<(), Box<dyn Error>> {
         Ok(())
+    }
+
+    pub async fn get_code(body: &String) -> Result<String, Box<dyn Error>> {
+        todo!()
     }
 }

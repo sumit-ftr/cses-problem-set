823a3c6c0ae68414ff022e351842bf04ad037318
Sumit Modak <152054612+sumit-ftr@users.noreply.github.com>
2024-04-05 22:48:04 +0530

    added 03-Dynamic_Programming/055-dice_combinations.rs

---
+++ 03-Dynamic_Programming/055-dice_combinations.rs
@@ -0,0 +1,47 @@
+use std::io::Write;
+
+fn main() {
+    let mut token = Scanner::new(std::io::stdin().lock());
+    let mut out = std::io::BufWriter::new(std::io::stdout().lock());
+    let n = token.next::<usize>();
+    let mut cnt = Vec::<u32>::with_capacity(n + 1);
+    cnt.extend([1, 1, 2, 4, 8, 16, 32]);
+    for i in 7..=n {
+        let mut sum = 0u32;
+        for j in i - 6..i {
+            sum = (sum + cnt[j]) % 1000000007;
+        }
+        cnt.push(sum);
+    }
+    writeln!(out, "{}", cnt[n]).unwrap();
+}
+
+pub struct Scanner<R> {
+    reader: R,
+    buffer: Vec<u8>,
+    iter: std::str::SplitAsciiWhitespace<'static>,
+}
+
+impl<R: std::io::BufRead> Scanner<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buffer: vec![],
+            iter: "".split_ascii_whitespace(),
+        }
+    }
+
+    pub fn next<T: std::str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.iter.next() {
+                return unsafe { token.parse().unwrap_unchecked() };
+            }
+            self.buffer.clear();
+            self.reader.read_until(b'\n', &mut self.buffer).unwrap();
+            self.iter = unsafe {
+                let slice = std::str::from_utf8_unchecked(&self.buffer);
+                std::mem::transmute(slice.split_ascii_whitespace())
+            }
+        }
+    }
+}
